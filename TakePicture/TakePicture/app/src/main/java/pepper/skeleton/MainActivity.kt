fe0356eb41de41edaf4747abf39fa995668d8fa0
MladenovicNikola <n.mladenovic@students.htl-leonding.ac.at>
2024-10-23 15:20:08 +0200

    Testversion

--- TakePicture/TakePicture/app/src/main/java/pepper/skeleton/MainActivity.kt
+++ TakePicture/TakePicture/app/src/main/java/pepper/skeleton/MainActivity.kt
@@ -1,37 +1,51 @@
 package pepper.skeleton
 
 import android.content.ContentValues.TAG
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.aldebaran.qi.Future
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
-import com.aldebaran.qi.sdk.builder.SayBuilder
 import com.aldebaran.qi.sdk.builder.TakePictureBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 import com.aldebaran.qi.sdk.`object`.camera.TakePicture
-import com.aldebaran.qi.sdk.`object`.conversation.Say
 import com.aldebaran.qi.sdk.`object`.image.EncodedImage
 import com.aldebaran.qi.sdk.`object`.image.EncodedImageHandle
 import java.nio.ByteBuffer
-import androidx.activity.compose.setContent as setContent
-
+import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import aws.sdk.kotlin.services.dynamodb.model.GetItemRequest
+import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
+import aws.sdk.kotlin.services.rekognition.RekognitionClient
+import aws.sdk.kotlin.services.rekognition.model.Image
+import aws.sdk.kotlin.services.rekognition.model.SearchFacesByImageRequest
+import aws.sdk.kotlin.services.rekognition.model.FaceMatch
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+import java.io.Console
+import java.io.File
+import java.io.FileInputStream
 
 class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     val onPepper = true
     private var qiContext: QiContext? = null
-
     lateinit var imageView: ImageView
 
-    override fun onCreate(savedInstanceState: Bundle?,) {
+    override fun onCreate(savedInstanceState: Bundle?, ) {
         super.onCreate(savedInstanceState)
-        QiSDK.register(this,this)
+        QiSDK.register(this, this)
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.picture_view)
@@ -58,8 +72,9 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     fun takePic() {
 
-        if(onPepper){
-            val takePictureFuture: Future<TakePicture> = TakePictureBuilder.with(qiContext).buildAsync()
+        if (onPepper) {
+            val takePictureFuture: Future<TakePicture> =
+                TakePictureBuilder.with(qiContext).buildAsync()
 
             val timestampedImageHandleFuture = takePictureFuture?.andThenCompose { takePicture ->
                 Log.i(TAG, "take picture launched!")
@@ -81,9 +96,54 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
                 Log.i(TAG, "PICTURE RECEIVED! ($pictureBufferSize Bytes)")
 
-                val pictureBitmap = BitmapFactory.decodeByteArray(pictureArray, 0, pictureBufferSize)
+                val pictureBitmap =
+                    BitmapFactory.decodeByteArray(pictureArray, 0, pictureBufferSize)
                 runOnUiThread { imageView.setImageBitmap(pictureBitmap) }
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    recognizeFace(pictureArray)
+                }
             }
+
         }
+    }
+
+    suspend fun recognizeFace(pepperPic: ByteArray): String {
+        val rekognition = RekognitionClient { region = "us-east-1" }
+        val dynamodb = DynamoDbClient { region = "us-east-1" }
+
+        try{
+
+            val response = rekognition.searchFacesByImage(
+                SearchFacesByImageRequest {
+                    collectionId = "oldpeople"
+                    image = Image { bytes = pepperPic }
+                }
+            )
+
+            val faceMatches = response.faceMatches
+            if (!faceMatches.isNullOrEmpty()) {
+                for (match: FaceMatch in faceMatches) {
+                    val faceId = match.face?.faceId
+                    val face = dynamodb.getItem(
+                        GetItemRequest {
+                            tableName = "facerecognition"
+                            key = mapOf(
+                                "RekognitionId" to AttributeValue.S(faceId!!)
+                            )
+                        }
+                    )
+
+                    if (face.item != null) {
+                        println("Found Person: ${face.item!!["FullName"]?.asS()}")
+                        return "Found Person: ${face.item!!["FullName"]?.asS()}"
+                    }
+                }
+            }
+            "Person cannot be recognized"
+        } catch (e: Exception) {
+            "Error occurred: ${e.message}"
+        }
+        return "Person not found"
     }
 }
